4c21b6f3335ba03e9bcd852669348e025e6dfcf4
behzad <behzad.khosravifar@gmail.com>
2019-05-19 16:42:11 +0430

    init project

---
+++ sample/Model.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using Microsoft.Data.Sqlite;
+namespace ConsoleApp.SQLite
+{
+    
+
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Url { get; set; }
+
+        public List<Post> Posts { get; set; }
+    }
+
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public int BlogId { get; set; }
+        public Blog Blog { get; set; }
+    }
+}
---
+++ sample/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+
+namespace ConsoleApp.SQLite
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // SQLCipher Encryption is applied to database using DBBrowser for SQLite.
+            // DBBrowser for SQLite is free and open source tool to edit the SQLite files. 
+            using (var db = new BloggingContext())            
+            {
+                db.Database.EnsureCreated(); // create new db, if that not exist!
+
+                db.Blogs.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+                var count = db.SaveChanges();
+                Console.WriteLine("{0} records saved to database", count);
+
+                Console.WriteLine("All blogs in database:");
+                foreach (var blog in db.Blogs)
+                {
+                    Console.WriteLine(" - {0}", blog.Url);
+                }
+            }
+
+            Console.ReadKey();
+        }        
+    }
+}
